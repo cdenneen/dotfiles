e22ca9624f69e55fa520b519df5bf1fb3b2cb7c8
Chris Denneen <cdenneen@gmail.com>
2019-10-23 14:53:08 -0400

    Added since YADR handles zprofile already

--- zsh.after/prompt.zsh
+++ zsh.after/prompt.zsh
@@ -3,3 +3,17 @@ POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(battery disk_usage ram ip load time)
 POWERLEVEL9K_HIDE_BRANCH_ICON=true
 #prompt powerlevel9k
 prompt minimal
+
+if [ "$USER" = "root" ]
+then
+  #export PROMPT='%{$fg_bold[magenta]%}%m %{$fg_bold[blue]%}# %b%f'
+  export PROMPT='%{$fg_bold[magenta]%}macbook %{$fg_bold[blue]%}# %b%f'
+elif [ -n "${SSH_CONNECTION}" ]
+then
+  #export PROMPT='%{$fg_bold[cyan]%}%m %{$fg_bold[blue]%}# %b%f'
+  export PROMPT='%{$fg_bold[cyan]%}macbook %{$fg_bold[blue]%}# %b%f'
+else
+  #export PROMPT='%{$fg_bold[green]%}%m %{$fg_bold[blue]%}# %b%f'
+  export PROMPT='%{$fg_bold[green]%}macbook %{$fg_bold[blue]%}# %b%f'
+fi
+export RPROMPT='%{$fg_bold[red]%}$(git_branch)%{$fg_bold[yellow]%}$(svn_branch)%b[%{$fg_bold[blue]%}%~%b%f]'
